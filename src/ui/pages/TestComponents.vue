6069f1df1af8001f104b22d887067b58b4c65243
AsterCasc <astercass@qq.com>
2024-10-23 18:10:47 +0800

    feat: mix style rule

--- src/ui/pages/TestComponents.vue
+++ src/ui/pages/TestComponents.vue
@@ -3,10 +3,11 @@
 
     <q-btn @click="switchTheme" color="primary" label="Switch Theme"/>
 
-    <q-separator class="base-component-separator" spaced="2rem" size=".12rem"/>
+    <q-separator class="component-separator-base" spaced="2rem" size="2px"/>
 
-    <div class="row">
-      <q-btn no-caps padding=".3rem .5rem" unelevated class="shadow-2 base-component-full">
+    <div class="row items-center">
+
+      <q-btn no-caps unelevated class="q-mx-md shadow-2 component-full-btn-std ">
         <div class="row items-center">
           <q-icon name="fa-solid fa-arrow-down" size=".9rem"/>
           <div class="q-mx-xs">
@@ -14,45 +15,63 @@
           </div>
         </div>
       </q-btn>
+
+      <q-btn no-caps unelevated class="q-mx-md component-outline-btn-std">
+        <div class="row items-center">
+          <q-icon name="fa-solid fa-arrow-up" size=".9rem"/>
+          <div class="q-mx-xs">
+            Upload
+          </div>
+        </div>
+      </q-btn>
+
+
+      <q-btn no-caps unelevated class="q-mx-md component-outline-btn-short">
+        <div class="row items-center">
+          <q-icon name="fa-solid fa-ellipsis" size=".9rem"/>
+        </div>
+      </q-btn>
+
+
     </div>
 
-    <q-separator class="base-component-separator" spaced="2rem" size="0.16rem"/>
+    <q-separator class="component-separator-base" spaced="2rem" size="2px"/>
 
     <div>
       This is Text
     </div>
 
-    <q-separator class="base-component-separator" spaced="2rem" size=".12rem"/>
+    <q-separator class="component-separator-base" spaced="2rem" size="2px"/>
 
     <h1>
       This is h1
     </h1>
 
-    <q-separator class="base-component-separator" spaced="2rem" size=".12rem"/>
+    <q-separator class="component-separator-base" spaced="2rem" size="2px"/>
 
     <h2>
       This is h2
     </h2>
 
-    <q-separator class="base-component-separator" spaced="2rem" size=".12rem"/>
+    <q-separator class="component-separator-base" spaced="2rem" size="2px"/>
 
     <h3>
       This is h3
     </h3>
 
-    <q-separator class="base-component-separator" spaced="2rem" size=".12rem"/>
+    <q-separator class="component-separator-base" spaced="2rem" size="2px"/>
 
     <h4>
       This is h4
     </h4>
 
-    <q-separator class="base-component-separator" spaced="2rem" size=".12rem"/>
+    <q-separator class="component-separator-base" spaced="2rem" size="2px"/>
 
     <h5>
       This is h5
     </h5>
 
-    <q-separator class="base-component-separator" spaced="2rem" size=".12rem"/>
+    <q-separator class="component-separator-base" spaced="2rem" size=".15rem"/>
 
     <h6>
       This is h6
