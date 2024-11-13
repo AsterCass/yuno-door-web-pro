49b2a258ffe050bb85bd0575848344a942780a62
AsterCasc <astercass@qq.com>
2024-11-13 10:36:46 +0800

    feat: main page top text

--- src/ui/pages/WebMain.vue
+++ src/ui/pages/WebMain.vue
@@ -3,26 +3,21 @@
 
     <cask-base-header/>
 
-    <div class="cask-main-top-container">
+    <div class="cask-main-top-container row justify-center">
       <q-img :no-native-menu="false" :ratio="16/9" fit="cover"
              :src="globalState.curThemeName !== 'dark' ?
              '/img/bg-1-fixed.jpg' : '/img/bg-2-fixed.jpg'" class="cask-main-bg-image">
       </q-img>
 
-      <div style="color: ghostwhite; margin: 3rem 25rem 0 25rem">
+      <div class="cask-main-top-content">
         <div style="font-size: 5rem">
           {{ $t('main_title') }}
         </div>
         <div style="font-size: 1.5rem" class="q-my-md">
           {{ $t('main_subtitle') }}
         </div>
-        <div style="font-size: 1.2rem">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet
-          porro.
-          Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet
-          porro.
-          Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
+        <div style="font-size: 1.1rem; opacity: .8">
+          {{ $t('main_top_content') }}
         </div>
       </div>
 
@@ -62,6 +57,13 @@ const hideScroller = ref(false)
   position: relative;
   min-height: 70rem;
   padding: 5rem 1rem 0 1rem;
+
+  .cask-main-top-content {
+    padding-top: 2rem;
+
+    color: var(--full-container-text-color);
+    width: 60%
+  }
 }
 
 .cask-main-bg-image {
