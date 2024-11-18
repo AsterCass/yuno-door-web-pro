f1d82167b4d461aff15601b1fb321f442a045b7e
AsterCasc <astercass@qq.com>
2024-11-18 18:09:08 +0800

    feat: footer layout

--- src/ui/views/CaskBaseFooter.vue
+++ src/ui/views/CaskBaseFooter.vue
@@ -19,15 +19,30 @@
   <!--    </ul>-->
   <!--  </div>-->
 
-  <div class="bottom-footer-base row items-center">
+  <div class="bottom-footer-base row items-center justify-center">
 
-    <div>
-      4324
+    <div class="row items-center">
+      &copy; 2020-{{ curYear }} astercasc.com 版权所有 互联网ICP备案：
+      <a target="_blank" href="https://beian.miit.gov.cn/">浙ICP备2022023127号</a>
     </div>
-    <q-separator vertical class="component-separator-in-container" size="1px" style="margin: 8px 16px"/>
 
-    <div>
-      432423
+    <q-separator vertical class="component-separator-in-container" size="1px" style="margin: 16px"/>
+
+    <div class="row items-center">
+      Github：
+      <a target="_blank" href="https://github.com/AsterCass?tab=repositories">AsterCass</a>
+    </div>
+
+    <q-separator vertical class="component-separator-in-container" size="1px" style="margin: 16px"/>
+
+    <div class="row items-center">
+      Mail: astercass@qq.com
+    </div>
+
+    <q-separator vertical class="component-separator-in-container" size="1px" style="margin: 16px"/>
+
+    <div class="row items-center">
+      QQ: 404919617
     </div>
 
   </div>
@@ -50,9 +65,14 @@ onMounted(() => {
 <style scoped lang="scss">
 
 .bottom-footer-base {
+  margin-top: 1rem;
   background-color: var(--full-container-background-color-dark);
   color: var(--full-container-text-color);
   min-height: 3rem;
+
+  a {
+    color: var(--full-container-text-color);
+  }
 
 }
 
