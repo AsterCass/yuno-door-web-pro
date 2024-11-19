dc65127f4ac4e6353d2ca3e5c2fdfe3d9ce371a3
AsterCasc <astercass@qq.com>
2024-11-19 15:05:25 +0800

    feat: footer build

--- src/ui/views/CaskBaseFooter.vue
+++ src/ui/views/CaskBaseFooter.vue
@@ -1,49 +1,115 @@
 <template>
-  <!--  <div class="footer">-->
-  <!--    <ul class="copyright">-->
-  <!--      <li>&copy; 2020-{{ curYear }} astercasc.com 版权所有 互联网ICP备案：-->
-  <!--        <a target="_blank" href="https://beian.miit.gov.cn/">浙ICP备2022023127号</a>-->
-  <!--      </li>-->
-  <!--      <li>-->
-  <!--        <q-icon name="eva-github"/>-->
-  <!--        Github: <a target="_blank" href="https://github.com/AsterCass?tab=repositories">AsterCass</a>-->
-  <!--      </li>-->
-  <!--      <li>-->
-  <!--        <q-icon name="fa-solid fa-envelope"/>-->
-  <!--        Mail: astercass@qq.com-->
-  <!--      </li>-->
-  <!--      <li>-->
-  <!--        <q-icon name="fa-brands fa-qq"/>-->
-  <!--        QQ: 404919617-->
-  <!--      </li>-->
-  <!--    </ul>-->
-  <!--  </div>-->
 
-  <div class="bottom-footer-base row items-center justify-center">
+  <div class="bottom-footer-base row justify-evenly">
 
-    <div class="row items-center">
-      &copy; 2020-{{ curYear }} astercasc.com 版权所有 互联网ICP备案：
-      <a target="_blank" href="https://beian.miit.gov.cn/">浙ICP备2022023127号</a>
+    <div class="bottom-footer-base-header column justify-between" style="max-width: 35rem">
+      <div style="font-size: 1.5rem">
+        astercasc.com
+      </div>
+      <div style="opacity: .5">
+        {{ $t('main_web_desc') }}
+      </div>
+      <div style="opacity: .85">
+        <div>
+          &copy; 2020-{{ curYear }} astercasc.com {{ $t('main-nav-ipc-pre') }}
+          <a target="_blank" href="https://beian.miit.gov.cn/">{{ $t('main-nav-ipc-center') }}</a>
+        </div>
+
+      </div>
     </div>
 
-    <q-separator vertical class="component-separator-in-container" size="1px" style="margin: 16px"/>
-
-    <div class="row items-center">
-      Github：
-      <a target="_blank" href="https://github.com/AsterCass?tab=repositories">AsterCass</a>
+    <div class="bottom-footer-base-header column">
+      <div class="q-mb-md" style="font-size: 1.2rem;opacity: .85">
+        {{ $t('main-nav-quick-link') }}
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5" @click="openLink('')">
+        {{ $t('main_articles') }}
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5" @click="openLink('')">
+        {{ $t('main_videos') }}
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5" @click="openLink('')">
+        {{ $t('main_board') }}
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5" @click="openLink('')">
+        {{ $t('main_chat_room') }}
+      </div>
     </div>
 
-    <q-separator vertical class="component-separator-in-container" size="1px" style="margin: 16px"/>
-
-    <div class="row items-center">
-      Mail：astercass@qq.com
+    <div class="bottom-footer-base-header column">
+      <div class="q-mb-md" style="font-size: 1.2rem;opacity: .85">
+        {{ $t('main-nav-tool') }}
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5" @click="openLink('')">
+        {{ $t('main_tools_time_con') }}
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5" @click="openLink('')">
+        {{ $t('main_tools_md5_gen') }}
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5" @click="openLink('')">
+        {{ $t('main_tools_base64_con') }}
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5" @click="openLink('')">
+        {{ $t('main_tools_rgb_con') }}
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5" @click="openLink('')">
+        {{ $t('main_tools_qrcode_gen') }}
+      </div>
     </div>
 
-    <q-separator vertical class="component-separator-in-container" size="1px" style="margin: 16px"/>
-
-    <div class="row items-center">
-      QQ：404919617
+    <div class="bottom-footer-base-header column">
+      <div class="q-mb-md" style="font-size: 1.2rem;opacity: .85">
+        {{ $t('main-nav-game') }}
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5" @click="openLink('')">
+        Hextris
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5" @click="openLink('')">
+        Mikutap
+      </div>
     </div>
+
+    <div class="bottom-footer-base-header column">
+      <div class="q-mb-md" style="font-size: 1.2rem;opacity: .85">
+        {{ $t('main-nav-personal-res') }}
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5"
+           @click="openLink('https://github.com/AsterCass?tab=repositories')">
+        GitHub
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5"
+           @click="openLink('https://github.com/AsterCass/Tomoyo')">
+        Tomoyo
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5"
+           @click="openLink('https://github.com/AsterCass/sql-garble-spring-boot')">
+        Mybatis Sql Garble
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5"
+           @click="openLink('https://github.com/AsterCass/yuno-door-web-pro')">
+        Yuno Web Door Pro
+      </div>
+      <div class="cask-jump-link-in-text-origin q-my-xs " style="opacity: .5"
+           @click="openLink('https://github.com/AsterCass/yuno-saas-door')">
+        Yuno Saas Door
+      </div>
+    </div>
+
+    <div class="bottom-footer-base-header column">
+      <div class="q-mb-md" style="font-size: 1.2rem;opacity: .85">
+        {{ $t('main-nav-contact-me') }}
+      </div>
+      <div class="q-my-xs https://github.com/AsterCass?tab=repositories" style="opacity: .5">
+        {{ $t('main-fa-mail') }}: astercass@qq.com
+      </div>
+      <div class="q-my-xs https://github.com/AsterCass/Tomoyo" style="opacity: .5">
+        {{ $t('main-fa-qq') }}: 4040191617
+      </div>
+      <div class="q-my-xs https://github.com/AsterCass/sql-garble-spring-boot" style="opacity: .5">
+        {{ $t('main-fa-wechat') }}: AsterCasc
+      </div>
+    </div>
+
 
   </div>
 </template>
@@ -52,6 +118,7 @@
 
 import {onMounted, ref} from "vue";
 import {date} from "quasar";
+import {openLink} from "@/utils/base-tools";
 
 const curYear = ref("2020")
 
@@ -66,9 +133,10 @@ onMounted(() => {
 
 .bottom-footer-base {
   margin-top: 1rem;
+  padding: 1.5rem 2rem 2rem 2rem;
   background-color: var(--full-container-background-color-dark);
   color: var(--full-container-text-color);
-  min-height: 3rem;
+  min-height: 16rem;
 
   a {
     color: var(--full-container-text-color);
--- src/utils/base-tools.js
+++ src/utils/base-tools.js
@@ -2,3 +2,13 @@ export function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
+export function openLink(url, newBlank = true) {
+    if (newBlank) {
+        window.open(url, "_blank");
+    } else {
+        window.location.href = url;
+    }
+
+}
+
+
