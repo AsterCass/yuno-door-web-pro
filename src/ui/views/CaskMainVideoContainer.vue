b0f11eecd2e13a99b4d2b736e2da49fbf3dd6117
AsterCasc <astercass@qq.com>
2024-11-26 10:50:36 +0800

    feat: videos finish

--- src/ui/views/CaskMainVideoContainer.vue
+++ src/ui/views/CaskMainVideoContainer.vue
@@ -13,25 +13,27 @@
 
     <div class="col-12 row justify-between q-mt-sm q-mb-lg items-center">
 
-      <q-btn no-caps unelevated class="component-none-btn-grow q-mx-xs q-ml-md"
+      <q-btn no-caps unelevated class="component-none-btn-grow q-mx-xs q-ml-md" @click="scrollVideoStep(false)"
              style="background-color: rgb(var(--full-container-background-color-light))">
         <div class="row items-center q-my-xl">
           <q-icon name="fa-solid fa-chevron-left" size="1.5rem" style="margin-top: 2px"/>
         </div>
       </q-btn>
 
-      <q-virtual-scroll :style="!globalState.screenMini ? 'width: 90%' : 'width: 60%'"
-                        class="component-no-scroller"
-                        virtual-scroll-item-size="0" virtual-scroll-horizontal
-                        v-slot="{index}" :items="videoList">
+      <div ref="videoScrollerRef" :style="!globalState.screenMini ? 'width: 88%' : 'width: 60%'">
+        <q-virtual-scroll
+            class="component-no-scroller"
+            virtual-scroll-item-size="0" virtual-scroll-horizontal
+            v-slot="{index}" :items="videoList">
+          <div :id="videoList[index].id" style="height: 320px; width: 180px;"
+               class="q-my-xl q-mx-md">
+            <cask-video-card :video-col-data="videoList[index]"/>
+          </div>
+        </q-virtual-scroll>
+      </div>
 
-        <div style="height: 20rem; width: 14rem; background-color: grey;" class="q-ma-md">
-          {{ videoList[index].id }}
-        </div>
-      </q-virtual-scroll>
 
-
-      <q-btn no-caps unelevated class="component-none-btn-grow q-mx-xs q-mr-md"
+      <q-btn no-caps unelevated class="component-none-btn-grow q-mx-xs q-mr-md" @click="scrollVideoStep(true)"
              style="background-color: rgb(var(--full-container-background-color-light))">
         <div class="row items-center q-my-xl">
           <q-icon name="fa-solid fa-chevron-right" size="1.5rem" style="margin-top: 2px"/>
@@ -61,20 +63,51 @@ import {onMounted, ref} from "vue";
 import {useGlobalStateStore} from "@/utils/global-state";
 import {getAllVideoCol} from "@/api/video";
 import {customPageNP} from "@/utils/page";
+import CaskVideoCard from "@/ui/views/CaskVideoCard.vue";
 
 
 const globalState = useGlobalStateStore();
 
+const videoCurrent = ref(0)
 const videoList = ref([])
+const videoScrollerRef = ref(null)
 
+function scrollVideoStep(isNext) {
+  //cal
+  let toVideoIndex = isNext ? videoCurrent.value + 1 : videoCurrent.value - 1
+  let rellyIndex = toVideoIndex > videoList.value.length - 1 ? videoList.value.length - 1 : toVideoIndex
+  rellyIndex = rellyIndex < 0 ? 0 : rellyIndex
+
+  //assign
+  const container = videoScrollerRef.value
+  if (container) {
+    const targets = container.querySelectorAll('.q-virtual-scroll');
+    if (targets.length > 0) {
+      //parent and child
+      const target = targets[0]
+      const maxScrollOffset = target.scrollWidth - target.clientWidth
+      const childTarget = document.getElementById(videoList.value[rellyIndex].id);
+      //offset
+      const targetLeft = target.getBoundingClientRect().left;
+      const childTargetLeft = childTarget.getBoundingClientRect().left;
+      let scrollOffset = childTargetLeft - targetLeft + target.scrollLeft;
+      //set
+      target.scrollTo({
+        left: (rellyIndex === 0) ? 0 : scrollOffset,
+        behavior: 'smooth',
+      })
+      if (scrollOffset < maxScrollOffset) {
+        videoCurrent.value = rellyIndex
+      }
+    }
+  }
+}
 
 onMounted(() => {
   getAllVideoCol(customPageNP(0, 10)).then(res => {
-    console.log(res)
     if (!res || !res.data) {
       return
     }
-    console.log(res.data)
     videoList.value.push(...res.data)
   })
 })
---
+++ src/ui/views/CaskVideoCard.vue
@@ -0,0 +1,116 @@
+<template>
+
+  <div class="video-col-frame-main">
+
+    <div v-if="isEmpty" class="video-col-card-new">
+      <q-btn class="row video-col-card-new-inner bg-grey-3" style="padding: 1%" label="UPLOAD"
+             icon="fa-solid fa-plus" text-color="green-10" stack size="25px" @click="uploadVideo"/>
+
+      <div class="text-center q-mt-sm video-col-title">
+        <div class="text-center text-grey-7 simple-content simple-a-no-underline">上传视频</div>
+      </div>
+
+    </div>
+
+    <div v-else class="video-col-card-list">
+
+      <q-btn class="video-col-card-img bg-white" @click="playVideoList">
+        <q-img :src="`${videoColData.collectionImg}`" :ratio="9/16" style="border-radius: 1rem">
+        </q-img>
+      </q-btn>
+      <div class="text-center video-col-card-title">
+        <div class="text-center text-grey-7 simple-content simple-a-no-underline">
+          {{ videoColData.collectionName }}
+        </div>
+      </div>
+
+    </div>
+
+
+  </div>
+
+
+</template>
+
+<script setup>
+import {defineProps} from "vue";
+
+//prop
+const props = defineProps({
+  isEmpty: {
+    type: Boolean,
+    default: false
+  },
+  videoColData: {
+    id: ""
+  }
+})
+
+function uploadVideo() {
+}
+
+function playVideoList() {
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+
+.video-col-frame-main {
+  width: 100%;
+  height: 100%;
+
+  .video-col-card-new {
+    width: 100%;
+    height: 92%;
+    border-radius: 1rem;
+
+    .video-col-card-new-inner {
+      height: 100%;
+      width: 100%;
+      box-shadow: 4px 4px 2px rgb(var(--container-text-color));
+      border-radius: 1rem;
+    }
+  }
+
+
+  .video-col-card-list {
+
+    .video-col-card-img {
+      //animation: fadeOutDown 1.7s infinite;
+      padding: 1%;
+      border-radius: 1rem;
+      box-shadow: 4px 4px 2px rgb(var(--container-text-color));
+      position: relative;
+      width: 92%;
+      left: 4%;
+      transition-duration: .3s;
+    }
+
+    .video-col-card-title {
+      transition-duration: .3s;
+      margin-top: 5%;
+    }
+  }
+
+  .video-col-card-list:hover {
+    .video-col-card-img {
+      margin: -20% -10%;
+      box-shadow: 8px 8px 8px rgb(var(--container-text-color));
+      width: 110%;
+    }
+
+    .video-col-card-title {
+      margin-top: 25%;
+      font-size: 1.2rem;
+    }
+
+
+  }
+
+
+}
+
+
+</style>
