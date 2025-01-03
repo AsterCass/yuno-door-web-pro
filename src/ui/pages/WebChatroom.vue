79805b8cc4ae5bafd0350bf1b7d87b94d0f4fb78
AsterCasc <astercass@qq.com>
2025-01-03 17:40:56 +0800

    fix: delete animate__animated for show emoji list background

--- src/ui/pages/WebChatroom.vue
+++ src/ui/pages/WebChatroom.vue
@@ -312,7 +312,7 @@
 
         <q-separator class="component-separator-base" vertical style="margin: 5rem 0 0 2%"/>
 
-        <div v-show="!openChatSetting" class="animate__animated animate__fadeIn">
+        <div v-show="!openChatSetting">
           <cask-long-text-input v-if="socketChatState.webChattingFocusChat" is-absolute
                                 id="comment-reply-input" :elements="new Map([
                   [CaskLongTextInputElement.FILE, {callback: ()=> {notifyTopWarning($t('in_develop'))}}],
