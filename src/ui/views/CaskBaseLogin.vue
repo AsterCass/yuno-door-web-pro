d566b9914c919ce64eaf75ed4164868b902ebeee
AsterCasc <astercass@qq.com>
2025-05-07 11:36:43 +0800

    fix: support github login get user mail

--- src/ui/views/CaskBaseLogin.vue
+++ src/ui/views/CaskBaseLogin.vue
@@ -166,7 +166,7 @@ const googleLoginUrl = "https://accounts.google.com/o/oauth2/v2/auth" +
     "&response_type=code&scope=email profile&access_type=offline"
 
 const githubLoginUrl = "https://github.com/login/oauth/authorize?" +
-    "client_id=Iv23li6nHiBxyapgxcmi&redirect_uri=https://www.astercasc.com/auth/github/callback&scope=read:user%20user:email"
+    "client_id=Iv23li6nHiBxyapgxcmi&redirect_uri=https://www.astercasc.com/auth/github/callback&scope=user user:email"
 const inputAccount = ref('')
 const inputPassword = ref('')
 const agreePrivacy = ref(false)
