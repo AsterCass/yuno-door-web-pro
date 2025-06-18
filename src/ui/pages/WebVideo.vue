94ed75b27c3b6a6442382f35fc619a440f92a450
AsterCasc <astercass@qq.com>
2025-06-18 15:51:58 +0800

    feat: video page init

---
+++ src/ui/pages/WebVideo.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-layout view="hhh lpr fff">
+
+    <cask-base-header :center-elements="[
+        CaskModuleElement.HOME,
+        CaskModuleElement.ARTICLE,
+        CaskModuleElement.CHATROOM,
+    ]"/>
+
+
+    <cask-base-footer/>
+
+  </q-layout>
+</template>
+
+<script setup>
+import {CaskModuleElement} from "@/constant/enums/component-enums";
+import CaskBaseHeader from "@/ui/views/CaskBaseHeader.vue";
+import CaskBaseFooter from "@/ui/views/CaskBaseFooter.vue";
+import {useGlobalStateStore} from "@/utils/global-state";
+import {useRouter} from "vue-router";
+import {useI18n} from "vue-i18n";
+
+const {t} = useI18n()
+const globalState = useGlobalStateStore();
+const thisRouter = useRouter()
+
+
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
