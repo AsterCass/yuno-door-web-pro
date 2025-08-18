9eb186602ebe6204a237674c08976997fbb3c04c
AsterCasc <astercass@qq.com>
2025-08-18 15:45:32 +0800

    feat: use plyr play, auto next, autoplay

--- src/ui/pages/WebVideoPlay.vue
+++ src/ui/pages/WebVideoPlay.vue
@@ -18,7 +18,7 @@
                        :thumb-style="globalState.curThemeName === 'dark' ?
                          { background: 'white', width: '6px' } :
                           { background: 'black', width: '6px' }">
-          <cask-tabs-vertical :tabs="tabs" v-model="tab" width="90%" text-width="60%"
+          <cask-tabs-vertical :tabs="tabs" v-model="tab" width="90%" text-width="60%" @update:model-value="updateVdo"
                               force-text-color="rgb(var(--text-color))" :i18n="false"/>
 
         </q-scroll-area>
@@ -31,7 +31,17 @@
         <div class="main-player-pre">
 
         </div>
-        <div id="mainPlayer" ref="mainPlayerRef" style="aspect-ratio: 16 / 9;"></div>
+
+        <div class="main-player">
+          <video
+              id="mainPlayer"
+              ref="mainPlayerRef"
+              class="plyr"
+              playsinline
+              controls
+          >
+          </video>
+        </div>
 
         <div class="main-player-post">
 
@@ -57,11 +67,12 @@ import {useGlobalStateStore} from "@/utils/global-state";
 import {useI18n} from "vue-i18n";
 import {defineProps, onBeforeUnmount, onMounted, ref} from "vue";
 import CaskTabsVertical from "@/ui/components/CaskTabsVertical.vue";
-import {notifyTopWarning} from "@/utils/notification-tools";
 import {getVideoListByColId} from "@/api/video";
-import Player from 'xgplayer';
+import Plyr from "plyr";
+import {toReplacePage, toSpecifyPage} from "@/router";
+import {useRouter} from "vue-router";
 
-
+const thisRouter = useRouter()
 const globalState = useGlobalStateStore();
 const {t} = useI18n()
 
@@ -79,35 +90,101 @@ const props = defineProps({
   },
 })
 // data
-let vdoListData = ref([])
+const vdoListData = ref([])
 const tabs = ref([])
 const tab = ref("base")
 const mainPlayerRef = ref(null)
-const currentVideoId = ref("");
+const currentVideoData = ref(
+    {
+      id: "",
+      videoRes: "",
+      videoName: "",
+      videoNum: 0,
+    }
+)
 
-let player = null;
+let player = null
 
-function resetPlayer(url) {
+function initPlayer() {
   if (mainPlayerRef.value && !player) {
-    player = new Player({
-      id: mainPlayerRef.value.id,
-      url: url,
-      width: '95%',
-      height: 'auto',
-    })
+    player = new Plyr(mainPlayerRef.value,
+        {
+          controls: [
+            'play-large', // The large play button in the center
+            'play', // Play/pause playback
+            'progress', // The progress bar and scrubber for playback and buffering
+            'current-time', // The current time of playback
+            'duration', // The full duration of the media
+            'mute', // Toggle mute
+            'volume', // Volume control
+            'captions', // Toggle captions
+            'settings', // Settings menu
+            'fullscreen', // Toggle fullscreen
+          ],
+          autoplay: true,
+          keyboard: {focused: true, global: true},
+        }
+    )
+    player.on('ended', () => {
+      if (vdoListData.value && vdoListData.value.length > 0) {
+        const nextCount = currentVideoData.value.videoNum
+        if (vdoListData.value.length > nextCount) {
+          currentVideoData.value = vdoListData.value[nextCount]
+          toReplacePage(thisRouter, {colId: props.colId, vdoId: currentVideoData.value.id})
+          tab.value = currentVideoData.value.id
+          resetPlayer(currentVideoData.value)
+        }
+      }
+    });
   }
+}
+
+function updateVdo(newVdoId) {
+  for (const vdo of vdoListData.value) {
+    if (vdo.id === newVdoId) {
+      currentVideoData.value = vdo
+      break;
+    }
+  }
+  resetPlayer(currentVideoData.value)
+}
+
+function resetPlayer(data) {
+  if (!mainPlayerRef.value || !player) {
+    return
+  }
+  player.source = {
+    type: "video",
+    sources: [
+      {
+        src: data.videoRes,
+        type: "video/mp4"
+      }
+    ]
+  };
 }
 
 onMounted(() => {
   if (!globalState.isLogin) {
-    notifyTopWarning(t('no_login'))
+    toSpecifyPage(thisRouter, "401")
     return
   }
+  initPlayer()
   let param = {collectionId: props.colId}
   getVideoListByColId(param).then(res => {
     if (!res || !res.data) {
       return
     }
+    if (res.data.length <= 0) {
+      toSpecifyPage(thisRouter, "403")
+      return
+    }
+    // set current video
+    if (!props.vdoId) {
+      toReplacePage(thisRouter, {colId: props.colId, vdoId: res.data[0].id})
+    }
+    currentVideoData.value = props.vdoId ? {id: props.vdoId} : res.data[0]
+    // load data
     vdoListData.value = res.data
     for (const vdo of vdoListData.value) {
       const newTab = {}
@@ -115,19 +192,20 @@ onMounted(() => {
       newTab.label = vdo.videoName
       newTab.color = 'rgb(var(--full-container-background-color))'
       tabs.value.push(newTab)
+      if (vdo.id === currentVideoData.value.id) {
+        currentVideoData.value = vdo
+      }
     }
-    tab.value = vdoListData.value[0].id
-
-    resetPlayer(vdoListData.value[0].videoRes)
-
+    tab.value = currentVideoData.value.id
+    // play
+    resetPlayer(currentVideoData.value)
   })
 })
 
 
 onBeforeUnmount(() => {
   if (player) {
-    player.destroy();
-    player = null
+    player.destroy()
   }
 })
 
@@ -135,7 +213,6 @@ onBeforeUnmount(() => {
 </script>
 
 <style scoped lang="scss">
-@import 'xgplayer/dist/index.min.css';
 
 .main-player-pre {
   height: 2rem;
@@ -149,6 +226,26 @@ onBeforeUnmount(() => {
   background: rgb(var(--full-container-background-color));
   border-radius: 0 0 8px 8px;
   width: 95%;
+}
+
+.main-player {
+  width: 95%;
+  aspect-ratio: 16 / 9;
+  background: black;
+}
+
+</style>
+
+<style lang="scss">
+@import "plyr/dist/plyr.css";
+
+:root {
+  --plyr-color-main: rgb(var(--full-container-background-color));
+}
+
+.plyr {
+  aspect-ratio: 16 / 9;
+  width: 100%;
 }
 
 </style>
